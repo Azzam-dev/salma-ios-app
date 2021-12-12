67fa2bb7b87abc549f38eb7436d59a2fdb7146d1
Azzam R Alrashed <azzam.rar@gmail.com>
2021-12-12 15:41:33 +0300

    Initial Commit

---
+++ Salma/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Salma
+//
+//  Created by spark inc on 08/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
